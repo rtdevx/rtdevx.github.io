6fb78bce4f0166f32dab13da83eb366c9786f048
robk <robk@localhost>
2026-06-12 18:48:10 +0100

    Obsidian sync: 2026-06-12 18:48:10

--- assets/js/toc-click.js
+++ assets/js/toc-click.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const tocLinks = document.querySelectorAll(".toc a");
+  const tocLinks = document.querySelectorAll(".toc-link");
 
   tocLinks.forEach(link => {
     link.addEventListener("click", () => {
-      tocLinks.forEach(l => l.classList.remove("toc-active"));
-      link.classList.add("toc-active");
+      tocLinks.forEach(l => l.classList.remove("toc-link-active"));
+      link.classList.add("toc-link-active");
     });
   });
 });
